081a8575ab28f7b4676674d4f3daf47685efda42
Khemmachat Thongkhum <khemmachattk@gmail.com>
2018-09-29 19:09:04 +0700

    Initial Commit

---
+++ awesomPhone/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  awesomPhone
+//
+//  Created by Khemmachat Thongkhum on 29/9/2561 BE.
+//  Copyright © 2561 Khemmachat Thongkhum. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
